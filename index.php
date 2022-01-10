e9e5b12e57d3bb313b9b5cb580bf49f7fab50151
Saulo Frauches <saulo_pf@hotmail.com>
2022-01-10 18:01:17 -0300

    Primeiro commit. Idealizando o projeto

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="pt-br">
+    
+    <head>
+        
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link href="style.css" rel="stylesheet" id="bootstrap-css">
+
+        <title>Hello, world!</title>
+        
+    </head>
+    
+    <body>
+   
+        <div class="main"> 
+            <div class="container">
+                <center>
+                    <div class="middle">
+                        <div id="login">
+                            <form action="javascript:void(0);" method="get">
+                                <fieldset class="clearfix">
+                                    <p>
+                                        <span class="fa fa-user"></span>
+                                        <input type="text"  Placeholder="Username" required>
+                                    </p> 
+                                    <!-- JS because of IE support; better: placeholder="Username" -->
+                                    <p>
+                                        <span class="fa fa-lock"></span>
+                                        <input type="password"  Placeholder="Password" required>
+                                    </p> <!-- JS because of IE support; better: placeholder="Password" -->
+
+                                    <div>
+                                        <span style="width:48%; text-align:left;  display: inline-block;">
+                                            <a class="small-text" href="#">Forgot password?</a>
+                                        </span>
+                                        <span style="width:50%; text-align:right;  display: inline-block;">
+                                            <input type="submit" value="Sign In">
+                                        </span>
+                                    </div>
+                                </fieldset>
+                                <div class="clearfix"></div>
+                            </form>
+                            <div class="clearfix"></div>
+                        </div> 
+                        <div class="logo">
+                            SPF.Chat
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </center>
+            </div>
+        </div>
+      
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
